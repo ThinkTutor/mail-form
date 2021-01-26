bafc1e66e2609a835aca7f3d92e9914addb220b6
Matthew Thomas <matthew@mwt.me>
2021-01-25 22:54:52 -0600

    add hcaptcha requirement

--- lambda.py
+++ lambda.py
@@ -2,9 +2,16 @@ import os
 import json
 import urllib3
 
-# Get secrets from Lambda environment variables
+# Get mailgun secret from Lambda environment variables
 mg_api = os.environ["MG_API"]
+# Get hCaptcha secret from Lambda environment variables
+h_captcha = os.environ["HC_API"]
 bcc_emails = os.environ["EMAILS"]
+
+# Use this url for hCaptcha
+hcURL = "https://hcaptcha.com/siteverify"
+# Use this url for mailgun
+mgURL = "https://api.mailgun.net/v3/mail.thinktutor.org/messages"
 
 # Import the automatic email from res/formMail.txt
 with open("formmail.txt", "r") as myfile:
@@ -14,30 +21,45 @@ with open("formmail.txt", "r") as myfile:
 def lambda_handler(event, context):
     # Import the submitted contact info
     in_dict = json.loads(event["body"])
+    # Get hCaptch token
+    h_token = in_dict['h-captcha-response']
     # Start pool manager
     http = urllib3.PoolManager()
-    # Use the secret API key for mailgun
-    headers = urllib3.util.make_headers(basic_auth="api:" + mg_api)
-    # Format the body of the request for mailgun
-    r_dict = {
-        "from": "Think Tutor <hello@thinktutor.org>",
-        "to": in_dict["name"] + "<" + in_dict["email"] + ">",
-        "bcc": bcc_emails,
-        "subject": "Thank you for contacting ThinkTutor",
-        "text": formMail
-        + "\n\n> "
-        + in_dict["body"].replace("\n", "\n> ")
-        + "\n>\n>---\n> "
-        + in_dict["name"]
-        + "\n> "
-        + in_dict["email"],
+    h_dict = {
+        'secret': h_captcha,
+        'response': h_token
     }
-    # Actually make the request
-    r = http.request(
+    h = http.request(
         method="POST",
-        url="https://api.mailgun.net/v3/mail.thinktutor.org/messages",
-        fields=r_dict,
-        headers=headers,
+        url=hcURL,
+        fields=h_dict,
     )
-    return {"statusCode": r.status, "body": r.data.decode("utf-8")}
+    captcha_response = json.loads(h.data.decode("utf-8"))
+    if captcha_response['success']:
+        # Use the secret API key for mailgun in header
+        headers = urllib3.util.make_headers(basic_auth="api:" + mg_api)
+        # Format the body of the request for mailgun
+        r_dict = {
+            "from": "Think Tutor <hello@thinktutor.org>",
+            "to": in_dict["name"] + "<" + in_dict["email"] + ">",
+            "bcc": bcc_emails,
+            "subject": "Thank you for contacting ThinkTutor",
+            "text": formMail
+            + "\n\n> "
+            + in_dict["body"].replace("\n", "\n> ")
+            + "\n>\n>---\n> "
+            + in_dict["name"]
+            + "\n> "
+            + in_dict["email"],
+        }
+        # Actually make the request
+        r = http.request(
+            method="POST",
+            url=mgURL,
+            fields=r_dict,
+            headers=headers,
+        )
+        return {"statusCode": r.status, "body": r.data.decode("utf-8")}
+    else:
+        return {"statusCode": h.status, "body": "captcha error"}
 
